3cd72fc2df2c9b666afa510274e61f7d4bb7108b
lalit <lalitrwagh2007@gmail.com>
2026-09-03 16:32:32 +0530

    GPSdata

--- src/context/LocationContext.jsx
+++ src/context/LocationContext.jsx
@@ -1,1 +1,36 @@
-// kdfjhbiguaysrh
+import { createContext, useState } from 'react';
+
+export const LocationContext = createContext();
+
+export const LocationProvider = ({ children }) => {
+  const [coordinates, setCoordinates] = useState(null);
+  const [locationError, setLocationError] = useState(null);
+
+  const requestLocation = () => {
+    setLocationError(null);
+
+    navigator.geolocation.getCurrentPosition(
+      (position) => {
+        setCoordinates({
+          latitude: position.coords.latitude,
+          longitude: position.coords.longitude,
+        });
+      },
+      (error) => {
+        setLocationError(error.message);
+      },
+    );
+  };
+
+  return (
+    <LocationContext.Provider
+      value={{
+        coordinates,
+        locationError,
+        requestLocation,
+      }}
+    >
+      {children}
+    </LocationContext.Provider>
+  );
+};
