c7a46bde3a5420df6736cafb3e7b2b9ffedd9cf4
lalit <lalitrwagh2007@gmail.com>
2026-09-03 16:29:48 +0530

    authContext

--- src/context/AuthContext.jsx
+++ src/context/AuthContext.jsx
@@ -1,1 +1,38 @@
-// kdfjhbiguaysrh
+import { createContext, useState } from 'react';
+import { loginUser } from '../services/mockApi.js';
+
+export const AuthContext = createContext();
+
+export const AuthProvider = ({ children }) => {
+  const [user, setUser] = useState(null);
+  const [isLoading, setIsLoading] = useState(false);
+
+  const login = async (email, password) => {
+    setIsLoading(true);
+
+    try {
+      const loggedInUser = await loginUser(email, password);
+      setUser(loggedInUser);
+      return loggedInUser;
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
+  const logout = () => {
+    setUser(null);
+  };
+
+  return (
+    <AuthContext.Provider
+      value={{
+        user,
+        isLoading,
+        login,
+        logout,
+      }}
+    >
+      {children}
+    </AuthContext.Provider>
+  );
+};
